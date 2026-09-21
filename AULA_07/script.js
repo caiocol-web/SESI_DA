790478e7ae267e700972f92c6ab41ccb631542f6
caiocolonetti <caiocolonetti@gmail.com>
2026-09-21 11:34:42 -0300

    upds

---
+++ AULA_07/script.js
@@ -0,0 +1,15 @@
+function login(){
+
+    const local_nome = localStorage.getItem("nome");
+    const local_senha = localStorage.getItem("senha");
+
+    const nome  = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+
+    if(nome == local_nome && senha == local_senha){
+        alert("Login realizado com sucesso");
+    }else{
+        alert("Login inválido");
+    }
+
+}
